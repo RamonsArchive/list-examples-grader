d0351e9a42ee2828a738318cd69b7cf017b19920
RamonsArchive <rmcdeem.m@gmail.com>
2024-02-14 16:03:27 -0800

    Update grade.sh
    grade.sh file updated

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,43 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+else 
+    echo "Missing ListExamples.java file."
+    exit 1
+fi 
+
+cd grading-area
+echo "We entered the grading-area directory"
+pwd 
+
+javac -cp $CPATH *.java
+
+
+if [[ $? -ne 0 ]]
+then 
+    echo "The program failed to compile. Try again"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+
+echo "Your score is $successes / $tests"
+echo "or $((successes/tests))"
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
